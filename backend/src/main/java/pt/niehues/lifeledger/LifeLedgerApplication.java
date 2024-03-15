19c2408f27e6df689cc3e24ef7eb220730e6e86c
caioniehues <cniehues1@gmail.com>
2024-03-15 15:45:32 +0000

    Request too large for gpt-4-0613 in organization org-Um4LsFvvjGMZD7RNhCCEgQSu on tokens per min (TPM): Limit 10000, Requested 130251. The input or output tokens must be reduced in order to run successfully. Visit https://platform.openai.com/account/rate-limits to learn more.

---
+++ backend/src/main/java/pt/niehues/lifeledger/LifeLedgerApplication.java
@@ -0,0 +1,13 @@
+package pt.niehues.lifeledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeLedgerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pt/niehues/lifeledger/LifeLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.niehues.lifeledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
